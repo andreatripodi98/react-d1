9c762fb00085f7484188494617e098f3791386f0
andreatripodi98 <andrea.oblio@gmail.com>
2025-07-21 15:42:24 +0200

    ready fr

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import ButtonComponent from "./assets/ButtonComponent";
+import ButtonComponent from "./ButtonComponent";
 import ImageComponent from "./ImageComponent";
 
 function App() {
--- src/ButtonComponent.jsx
+++ src/ButtonComponent.jsx
@@ -1,4 +1,4 @@
-function ButtonComponent({text}) {
-    return <button className="btn btn-warning">{text}</button>;
+function ButtonComponent({ text }) {
+  return <button className="btn btn-warning">{text}</button>;
 }
 export default ButtonComponent;
